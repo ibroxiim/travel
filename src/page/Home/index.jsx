1f3dc4631bbe5a97d4d886e5abe40d9d6155a424
WebCodersDeveloper <ibrohimahmedov869@gmail.com>
2025-01-25 13:53:08 +0500

    upt

--- src/page/Home/index.jsx
+++ src/page/Home/index.jsx
@@ -1,7 +1,24 @@
+import { ChevronDown } from "lucide-react";
+import "./home-style.scss";
+
 export default function Home() {
   return (
     <>
-      <section className="hero"></section>
+      <section className="hero">
+        <div className="hero_main">
+          <span>
+            THE COUNTER: <p>70</p> Countries <p>1036</p> Cities
+          </span>
+          <h2>Leave your mark on all over the world</h2>
+          <button>Read More</button>
+          <div className="scroll">
+            <p>Scroll Down to Continue</p>
+            <button className="scroll_icon">
+              <ChevronDown />
+            </button>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
